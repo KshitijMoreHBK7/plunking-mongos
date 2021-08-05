f10153de90dd70aec96baf2adac016a860830e07
Saru <75667315+Saruwhitehatjr@users.noreply.github.com>
2021-08-05 19:13:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ var launcher;
 
 function preload(){
 	boy=loadImage("images/boy.png");
+	tree=loadImage("images/tree.png");
 }
 
 function setup() {
@@ -25,7 +26,7 @@ function setup() {
 	stone = new Stone(235,420,30); 
 
 	mango1 = new Mango(1100,100,30);
-  mango2 = new Mango(1170,130,30);
+        mango2 = new Mango(1170,130,30);
 	mango3 = new Mango(1010,140,30);
 	mango4 = new Mango(1000,70,30);
 	mango5 = new Mango(1100,70,30);
@@ -37,7 +38,7 @@ function setup() {
 	mango11 = new Mango(1120,50,40);
 	mango12 = new Mango(900,160,40);
 
-	tree = new Tree(1050,580);
+	
 	ground = new Ground(width/2,600,width,20);
 
   //create launcher with stone as bodyA
@@ -55,7 +56,7 @@ function draw() {
   image(boy ,200,340,200,300);
   
 
-  tree.display();
+  
   stone.display();
 
 
